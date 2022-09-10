52f411e5be5848b36b951fdea24fe4d3448224ef
Jake Curragh <jcurragh03@gmail.com>
2022-09-10 12:29:34 +0000

    Quickstart

---
+++ index.js
@@ -0,0 +1,24 @@
+const { createCanvas, loadImage } = require('canvas')
+const fs = require('fs')
+const canvas = createCanvas(200, 200)
+const ctx = canvas.getContext('2d')
+
+// Write "Awesome!"
+ctx.font = '30px Impact'
+ctx.rotate(0.1)
+ctx.fillText('Awesome!', 50, 100)
+
+// Draw line under text
+var text = ctx.measureText('Awesome!')
+ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+ctx.beginPath()
+ctx.lineTo(50, 102)
+ctx.lineTo(50 + text.width, 102)
+ctx.stroke()
+
+// Draw cat with lime helmet
+loadImage('example.jpg').then((image) => {
+  ctx.drawImage(image, 50, 0, 70, 70)
+  const buffer = canvas.toBuffer('image/png')
+  fs.writeFileSync('./test.png', buffer)
+})
