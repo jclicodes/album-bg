5bdb5403d7475bec270857241fd5186abe8664d7
Jake Curragh <jcurragh03@gmail.com>
2022-09-10 15:30:30 +0000

    feat: now accepts 1 img argument

---
+++ colour.js
@@ -0,0 +1,47 @@
+const { createCanvas, loadImage } = require('canvas')
+const fs = require('fs')
+const Vibrant = require('node-vibrant')
+
+const RESOLUTION = [2560, 1440]
+const IMAGESIZE = [800, 800]
+
+const canvas = createCanvas(RESOLUTION[0], RESOLUTION[1])
+const ctx = canvas.getContext('2d')
+
+const getAlbumColours = async (image) => {
+  let v = new Vibrant(image)
+  return v.getPalette().then((palette) => {
+    const swatches = Object.keys(palette)
+    const randomSwatch = swatches[(Math.floor(Math.random() * swatches.length))]
+    const randomBg = palette[randomSwatch].getHex()
+
+    return randomBg
+  })
+}
+
+const drawAlbumCover = async (img) => {
+  const [imgWidth, imgHeight] = IMAGESIZE
+  const [width, height] = RESOLUTION
+
+  const x = width / 2 - imgWidth / 2
+  const y = height/ 2 - imgHeight / 2
+
+  loadImage(img).then(image => {
+    ctx.drawImage(image, x, y, imgWidth, imgHeight)
+  })
+}
+
+const drawImage = async (bgColour, img) => {
+  const [width, height] = RESOLUTION
+
+  ctx.fillStyle = bgColour
+  ctx.fillRect(0, 0, width, height) 
+
+  await drawAlbumCover(img)
+
+  const buffer = canvas.toBuffer('image/png')
+  fs.writeFileSync('test.png', buffer)
+}
+
+module.exports = { getAlbumColours, drawImage }
+
--- index.js
+++ index.js
@@ -1,49 +1,14 @@
-const { createCanvas, loadImage } = require('canvas')
-const fs = require('fs')
-const Vibrant = require('node-vibrant')
+#!/usr/bin/env node
+const { getAlbumColours, drawImage } = require('./colour.js')
+const yargs = require('yargs')
 
-const RESOLUTION = [2560, 1440]
-const IMAGE = "example.jpg"
-const IMAGESIZE = [800, 800]
-
-const canvas = createCanvas(RESOLUTION[0], RESOLUTION[1])
-const ctx = canvas.getContext('2d')
-
-const getAlbumColours = async (image) => {
-  let v = new Vibrant(image)
-  return v.getPalette().then((palette) => {
-    const swatches = Object.keys(palette)
-    const randomSwatch = swatches[(Math.ceil(Math.random() * swatches.length))]
-    const randomBg = palette[randomSwatch].getHex()
-
-    return randomBg
-  })
-}
-
-const drawAlbumCover = async (img) => {
-  const [imgWidth, imgHeight] = IMAGESIZE
-  const [width, height] = RESOLUTION
-
-  const x = width / 2 - imgWidth / 2
-  const y = height/ 2 - imgHeight / 2
-
-  loadImage(img).then(image => {
-    ctx.drawImage(image, x, y, imgWidth, imgHeight)
-  })
-}
-
-const drawImage = async (bgColour) => {
-  const [width, height] = RESOLUTION
-
-  ctx.fillStyle = bgColour
-  ctx.fillRect(0, 0, width, height) 
-
-  await drawAlbumCover(IMAGE)
-
-  const buffer = canvas.toBuffer('image/png')
-  fs.writeFileSync('test.png', buffer)
-}
+const argv = yargs(process.argv.slice(2))
+  .option('img', {
+    alias: 'i',
+    demandOption: true,
+    description: 'Single image or directory of images to use',
+    type: 'string',
+  }).argv
 
 
-getAlbumColours(IMAGE).then(randomBg => drawImage(randomBg))
-
+getAlbumColours(argv.img).then(randomBg => drawImage(randomBg, argv.img))
