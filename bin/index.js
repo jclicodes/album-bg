0cc6cb857ded112fcb849b68a225173edcc2e215
Jake Curragh <jcurragh03@gmail.com>
2022-09-10 18:57:47 +0000

    chore: normalize file tree into node utility package

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-const { getAlbumColours, drawImage } = require('./colour.js')
+
+const { getAlbumColours, drawImage } = require('../src/colour.js')
 const yargs = require('yargs')
 
 const argv = yargs(process.argv.slice(2))
