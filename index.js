430eec94b851b1d66225a3cd61370b875df4d8a2
Jake Curragh <jcurragh03@gmail.com>
2022-09-10 15:00:27 +0000

    chore: remove commented out code

--- index.js
+++ index.js
@@ -47,22 +47,3 @@ const drawImage = async (bgColour) => {
 
 getAlbumColours(IMAGE).then(randomBg => drawImage(randomBg))
 
-// // Write "Awesome!"
-// ctx.font = '30px Impact'
-// ctx.rotate(0.1)
-// ctx.fillText('Awesome!', 50, 100)
-//
-// // Draw line under text
-// var text = ctx.measureText('Awesome!')
-// ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-// ctx.beginPath()
-// ctx.lineTo(50, 102)
-// ctx.lineTo(50 + text.width, 102)
-// ctx.stroke()
-//
-// // Draw cat with lime helmet
-// loadImage('example.jpg').then((image) => {
-//   ctx.drawImage(image, 50, 0, 70, 70)
-//   const buffer = canvas.toBuffer('image/png')
-//   fs.writeFileSync('./test.png', buffer)
-// })
