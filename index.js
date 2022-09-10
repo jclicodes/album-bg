2252b12009330509a53e1620465f0a86593a2a82
Jake Curragh <jcurragh03@gmail.com>
2022-09-10 13:25:11 +0000

    feat: handle getting colours from album image

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 const { createCanvas, loadImage } = require('canvas')
-const fs = require('fs')
-const canvas = createCanvas(200, 200)
+const Vibrant = require('node-vibrant')
+
+const RESOLUTION = ["2560", "1440"]
+const IMAGE = "example.jpg"
+
+const canvas = createCanvas(RESOLUTION[0], RESOLUTION[1])
 const ctx = canvas.getContext('2d')
 
-// Write "Awesome!"
-ctx.font = '30px Impact'
-ctx.rotate(0.1)
-ctx.fillText('Awesome!', 50, 100)
+const getAlbumColours = () => {
+  Vibrant.from(IMAGE)
+    .quality(1)
+    .clearFilters()
+    .getPalette()
+    .then(palette => {
+      const swatches = Object.keys(palette)
+      const randomSwatch = swatches[(Math.ceil(Math.random() * swatches.length))]
+      const randomBg = palette[randomSwatch].getHex()
 
-// Draw line under text
-var text = ctx.measureText('Awesome!')
-ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-ctx.beginPath()
-ctx.lineTo(50, 102)
-ctx.lineTo(50 + text.width, 102)
-ctx.stroke()
+      return randomBg
+    })
+    // TODO: Exception if colours cannot be extracted
+  return ''
+}
 
-// Draw cat with lime helmet
-loadImage('example.jpg').then((image) => {
-  ctx.drawImage(image, 50, 0, 70, 70)
-  const buffer = canvas.toBuffer('image/png')
-  fs.writeFileSync('./test.png', buffer)
-})
+
+
+
+
+
+
+// // Write "Awesome!"
+// ctx.font = '30px Impact'
+// ctx.rotate(0.1)
+// ctx.fillText('Awesome!', 50, 100)
+//
+// // Draw line under text
+// var text = ctx.measureText('Awesome!')
+// ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+// ctx.beginPath()
+// ctx.lineTo(50, 102)
+// ctx.lineTo(50 + text.width, 102)
+// ctx.stroke()
+//
+// // Draw cat with lime helmet
+// loadImage('example.jpg').then((image) => {
+//   ctx.drawImage(image, 50, 0, 70, 70)
+//   const buffer = canvas.toBuffer('image/png')
+//   fs.writeFileSync('./test.png', buffer)
+// })
